2eb5504231e2c67e609f1e188ba27dc7b4909881
Durran Jordan <durran@gmail.com>
2016-10-15 18:49:27 -0400

    Keep locales in lower case

--- test/i18n.test.js
+++ test/i18n.test.js
@@ -8,7 +8,7 @@ describe('I18n', () => {
   describe('#currentLocale', () => {
     context('when the locale is not set', () => {
       it('defaults to en-GB', () => {
-        expect(i18n.currentLocale()).to.equal('en-GB');
+        expect(i18n.currentLocale()).to.equal('en-gb');
       });
     });
   });
@@ -19,7 +19,7 @@ describe('I18n', () => {
     });
 
     it('sets the locale', () => {
-      expect(i18n.currentLocale()).to.equal('en-US');
+      expect(i18n.currentLocale()).to.equal('en-us');
     });
   });
 
@@ -27,11 +27,15 @@ describe('I18n', () => {
     context('when passed a directory', () => {
       before((done) => {
         i18n.load(LOCALE_DIR, done);
-      });
+      })
 
       it('loads all the locales in the directory', () => {
         expect(i18n.translations['en-us'].test.section.hello).to.equal('world');
       });
     });
+  });
+
+  describe('#currentTranslations', () => {
+
   });
 });
