3fd53ce7c3be48d39de58f1bf0dfe88610e701b4
Durran Jordan <durran@gmail.com>
2016-10-15 18:17:35 -0400

    Initial commit and setup

---
+++ test/i18n.test.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const expect = require('chai').expect;
+const i18n = require('../index');
+
+const LOCALE_DIR = path.join(__dirname, 'locales');
+
+describe('I18n', () => {
+  describe('#currentLocale', () => {
+    context('when the locale is not set', () => {
+      it('defaults to en-GB', () => {
+        expect(i18n.currentLocale()).to.equal('en-GB');
+      });
+    });
+  });
+
+  describe('#setLocale', () => {
+    before(() => {
+      i18n.setLocale('en-US');
+    });
+
+    it('sets the locale', () => {
+      expect(i18n.currentLocale()).to.equal('en-US');
+    });
+  });
+
+  describe('#load', () => {
+    context('when passed a directory', () => {
+      before(() => {
+        i18n.load(LOCALE_DIR);
+      });
+
+      it('loads all the locales in the directory', () => {
+      });
+    });
+  });
+});
