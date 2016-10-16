a2345570a335256ad9f41dc3c4385e794f0d505f
Durran Jordan <durran@gmail.com>
2016-10-16 16:15:20 -0400

    Dont fail if directories dont exist

--- test/i18n.test.js
+++ test/i18n.test.js
@@ -34,68 +34,82 @@ describe('I18n', () => {
 
   describe('#load', () => {
     context('when passed a directory', () => {
-      context('when the locale exists', () => {
-        const i18n = new I18n('de');
+      context('when the directory exists', () => {
+        context('when the locale exists', () => {
+          const i18n = new I18n('de');
 
-        context('when loading new translations', () => {
-          before((done) => {
-            i18n.load(LOCALES_1, done);
-          })
+          context('when loading new translations', () => {
+            before((done) => {
+              i18n.load(LOCALES_1, done);
+            })
 
-          it('loads only the set locale from the directory', () => {
-            expect(i18n.t.test.section.hello).to.equal('Welt');
+            it('loads only the set locale from the directory', () => {
+              expect(i18n.t.test.section.hello).to.equal('Welt');
+            });
+          });
+
+          context('when loading additional translations', () => {
+            before((done) => {
+              i18n.load(LOCALES_2, done);
+            })
+
+            it('does not overwrite the original translations', () => {
+              expect(i18n.t.test.section.hello).to.equal('Welt');
+            });
+
+            it('merges the additional translations', () => {
+              expect(i18n.t.test.section.thing).to.equal('Ding');
+            });
           });
         });
 
-        context('when loading additional translations', () => {
-          before((done) => {
-            i18n.load(LOCALES_2, done);
-          })
+        context('when the locale does not exist', () => {
+          context('when a fallback locale was provided', () => {
+            const i18n = new I18n('es', 'de');
 
-          it('does not overwrite the original translations', () => {
-            expect(i18n.t.test.section.hello).to.equal('Welt');
+            before((done) => {
+              i18n.load(LOCALES_1, done);
+            })
+
+            it('loads the fallback translations', () => {
+              expect(i18n.t.test.section.hello).to.equal('Welt');
+            });
           });
 
-          it('merges the additional translations', () => {
-            expect(i18n.t.test.section.thing).to.equal('Ding');
+          context('when no fallback locale was provided', () => {
+            const i18n = new I18n('es');
+
+            before((done) => {
+              i18n.load(LOCALES_1, done);
+            })
+
+            it('falls back to the default', () => {
+              expect(i18n.t.test.section.hello).to.equal('world');
+            });
+          });
+
+          context('when no fallback translations exist', () => {
+            const i18n = new I18n('es', 'fr');
+
+            it('returns an error', (done) => {
+              i18n.load(LOCALES_1, (error) => {
+                expect(error.message).to.include('fr.yml');
+                done();
+              });
+            });
           });
         });
       });
 
-      context('when the locale does not exist', () => {
-        context('when a fallback locale was provided', () => {
-          const i18n = new I18n('es', 'de');
+      context('when the directory does not exist', () => {
+        const i18n = new I18n('es', 'fr');
 
-          before((done) => {
-            i18n.load(LOCALES_1, done);
-          })
+        before((done) => {
+          i18n.load('./does-not-exist', done);
+        })
 
-          it('loads the fallback translations', () => {
-            expect(i18n.t.test.section.hello).to.equal('Welt');
-          });
-        });
-
-        context('when no fallback locale was provided', () => {
-          const i18n = new I18n('es');
-
-          before((done) => {
-            i18n.load(LOCALES_1, done);
-          })
-
-          it('falls back to the default', () => {
-            expect(i18n.t.test.section.hello).to.equal('world');
-          });
-        });
-
-        context('when no fallback translations exist', () => {
-          const i18n = new I18n('es', 'fr');
-
-          it('returns an error', (done) => {
-            i18n.load(LOCALES_1, (error) => {
-              expect(error.message).to.include('fr.yml');
-              done();
-            });
-          });
+        it('skips the directory', () => {
+          expect(i18n.t.test).to.equal(undefined);
         });
       });
     });
